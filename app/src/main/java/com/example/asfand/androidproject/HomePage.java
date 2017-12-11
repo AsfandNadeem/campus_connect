8556e80bd4c502abe307cacf5559d98315c316d7
AsfandNadeem <asfandnadeem116@gmail.com>
2017-12-11 12:21:46 +0500

    layout

---
+++ app/src/main/java/com/example/asfand/androidproject/HomePage.java
@@ -0,0 +1,134 @@
+package com.example.asfand.androidproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class HomePage extends AppCompatActivity
+
+        implements NavigationView.OnNavigationItemSelectedListener {
+    ListView myListView;
+    ArrayList<ListShow> myRowItems;
+    CustomAdapter myAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        myListView=(ListView) findViewById(R.id.listfeeds) ;
+
+        myRowItems=new ArrayList<ListShow>();
+        myAdapter=new CustomAdapter(getApplicationContext(),myRowItems);
+        myListView.setAdapter(myAdapter);
+        setSupportActionBar(toolbar);
+        fillArrayList();
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void fillArrayList() {
+
+        ListShow row_one = new ListShow( );
+        row_one.setCategory("General");
+        row_one.setDate("30-Nov-2017");
+        row_one.setDescription("Fall 15 Sports Gala");
+        row_one.setName("Abbas Nazar");
+
+        ListShow row_two = new ListShow( );
+        row_two.setCategory("General");
+        row_two.setDate("30-Nov-2017");
+        row_two.setDescription("Fall 15 Sports Gala");
+        row_two.setName("Abbas Nazar");
+
+
+        myRowItems.add( row_one );
+        myRowItems.add( row_two );
+
+
+    }
+}
---
+++ app/src/main/java/com/example/asfand/androidproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.asfand.androidproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginClick(View view)
+    {
+        Intent i = new Intent(this, HomePage.class);
+        startActivity(i);
+
+    }
+
+    public void SignUpClick(View view)
+    {
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+
+    }
+}
